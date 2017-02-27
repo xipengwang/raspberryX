a09c2b2d7df9e34cd4895adcc2a5213c22b2b942
liuyanqi <liuyanqi@umich.edu>
2017-02-27 12:18:00 -0500

    pwm wip

--- src/rpi/pwm.c
+++ src/rpi/pwm.c
@@ -1,2 +1,66 @@
 #include "pwm.h"
 volatile rpi_pwm_t *rpi_pwm;
+
+
+void pwm_init(uint8_t channel, uint8_t markspace, uint8_t enable){
+uint32_t control = mem_read_32((uint32_t*)rpi_pwm);   
+ if(channel ==0){
+	if(markspace){
+	    control|= RPI_PWM_CTL_MSEN1;
+         }
+	else{
+            control &=~RPI_PWM_CTL_MSEN1;
+        }
+        if(enable){
+	    control |= RPI_PWM_CTL_PWEN1;
+        }
+        else{
+            control &=~ RPI_PWM_CTL_PWEN1;
+        }
+}
+ else if(channel ==1){
+	if(markspace){
+	    control|= RPI_PWM_CTL_MSEN2;
+         }
+	else{
+            control &=~RPI_PWM_CTL_MSEN2;
+        }
+        if(enable){
+	    control |= RPI_PWM_CTL_PWEN2;
+        }
+        else{
+            control &=~ RPI_PWM_CTL_PWEN2;
+        }
+}
+ mem_write_32_nb((uint32_t*)&rpi_pwm->CTL,control);
+
+}
+
+void pwm_set_clock(uint32_t divider){
+
+}
+
+void pwm_set_range(uint8_t channel, uint32_t range){
+ if(channel ==0){
+	mem_write_32_nb((uint32_t*)&rpi_pwm->RNG1, range);
+  }
+ else if(channel ==1){
+	mem_write_32_nb((uint32_t*)&rpi_pwm->RNG2, range);
+ }
+}
+
+void pwm_set_data(uint8_t channel, uint32_t data){
+ if(channel ==0){
+	mem_write_32_nb((uint32_t*)&rpi_pwm->DAT1, data);
+  }
+ else if(channel ==1){
+	mem_write_32_nb((uint32_t*)&rpi_pwm->DAT2, data);
+ }
+}
+
+
+
+
+
+
+
--- src/rpi/pwm.h
+++ src/rpi/pwm.h
@@ -2,6 +2,26 @@
 #define _RPI_PWM_H
 
 #include "rpi.h"
+typedef enum{
+    RPI_PWM_CTL_PWEN1 = 1,
+    RPI_PWM_CTL_MODE1 = 1<<1,
+    RPI_PWM_CTL_RPLT1 = 1<<2,
+    RPI_PWM_CTL_SBIT1 = 1<<3,
+    RPI_PWM_CTL_POLA1 = 1<<4,
+    RPI_PWM_CTL_FIFO1 = 1<<5,
+    RPI_PWM_CTL_CLRF1 = 1<<6,
+    RPI_PWM_CTL_MSEN1 = 1<<7,
+
+    RPI_PWM_CTL_PWEN2 = 1<<8,
+    RPI_PWM_CTL_MODE2 = 1<<9,
+    RPI_PWM_CTL_RPLT2 = 1<<10,
+    RPI_PWM_CTL_SBIT2 = 1<<11,
+    RPI_PWM_CTL_POLA2 = 1<<12,
+    RPI_PWM_CTL_FIFP2 = 1<<13,
+   //RESERVE 0
+    RPI_PWM_CTL_MSEN2 = 1<<15
+    
+} rpi_pwm_CTL_t;
 
 
 typedef struct {
@@ -18,7 +38,13 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
+void pwm_init(uint8_t channel, uint8_t markspace, uint8_t enable);
 
+void pwm_set_clock(uint32_t divider);
+
+void pwm_set_range(uint8_t channel, uint32_t range);
+
+void pwm_set_data(uint8_t channel, uint32_t data);
 
 #ifdef __cplusplus
 }
