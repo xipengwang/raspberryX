5653e26ad2f2a2518d46aeb66f94d55d8cd7a429
Xipeng Wang <xipengw@umich.edu>
2017-03-24 14:06:18 -0400

    WIP on SPI

--- src/rpi/spi.c
+++ src/rpi/spi.c
@@ -38,3 +38,40 @@ void rpi_spi_set_clk_divider(volatile rpi_spi_t *rpi_spi0, uint16_t _divider)
     }
     rpi_spi0->CLK.bit.CDIV = divider;
 }
+
+/* Writes (and reads) an number of bytes to SPI */
+void rpi_spi_transfernb(volatile rpi_spi_t *rpi_spi0, char* tbuf, char* rbuf, uint32_t len)
+{
+    uint32_t TXCnt = 0;
+    uint32_t RXCnt = 0;
+
+    //clear FIFO
+    rpi_spi0->CS.bit.CLEAR = 0x03;
+
+    // Set TA = 1; Transfer active.
+    rpi_spi0->CS.bit.TA = 1;
+
+    //TODO: Timeout
+    while ((TXCnt < len)||(RXCnt < len)) {
+        // TX fifo not full, so add some more bytes
+        // We write byte by byte into FIFO, not word by word
+        while (rpi_spi0->CS.bit.TXD && (TXCnt < len)) {
+            rpi_spi0->FIFO.reg = tbuf[TXCnt];
+            TXCnt++;
+        }
+        // Rx fifo not empty, so get the next received bytes
+        if (rbuf) {
+            while(rpi_spi0->CS.bit.RXD &&( RXCnt < len )) {
+                rbuf[RXCnt] = rpi_spi0->FIFO.reg;
+                RXCnt++;
+            }
+        } else {
+            RXCnt = len;
+        }
+    }
+    // Wait for DONE to be set
+    while (rpi_spi0->CS.bit.DONE);
+
+    // Set TA = 0
+    rpi_spi0->CS.bit.TA = 0;
+}
