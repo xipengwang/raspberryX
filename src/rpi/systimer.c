2a1985448cadf2999781ec2bcfb50750a4648393
Xipeng Wang <xipengw@umich.edu>
2017-03-24 09:33:17 -0400

    Clean up system timer functions (#21)

--- src/rpi/systimer.c
+++ src/rpi/systimer.c
@@ -1,36 +1,36 @@
 #include "systimer.h"
 volatile rpi_sys_timer_t *rpi_sys_timer;
 
-void rpi_st_clr_status(uint8_t timer)
+void rpi_st_clr_status(Rpi_Sys_Timer timer)
 {
-    rpi_sys_timer->CS |= timer;
+    rpi_sys_timer->CS.reg |= timer;
 }
 
-uint32_t rpi_st_get_status(uint8_t timer)
+uint32_t rpi_st_get_status(Rpi_Sys_Timer timer)
 {
-    return rpi_sys_timer->CS & timer;
+    return rpi_sys_timer->CS.reg & timer;
 }
 
-void rpi_st_set_cmp(uint8_t timer, uint32_t value)
+void rpi_st_set_cmp(Rpi_Sys_Timer timer, uint32_t value)
 {
     switch(timer){
         case SYSTEM_TIMER_M0:
-            rpi_sys_timer->C0 = value;
+            rpi_sys_timer->C0.reg = value;
             break;
         case SYSTEM_TIMER_M1:
-            rpi_sys_timer->C1 = value;
+            rpi_sys_timer->C1.reg = value;
             break;
         case SYSTEM_TIMER_M2:
-            rpi_sys_timer->C2 = value;
+            rpi_sys_timer->C2.reg = value;
             break;
         case SYSTEM_TIMER_M3:
-            rpi_sys_timer->C3 = value;
+            rpi_sys_timer->C3.reg = value;
             break;
     }
 }
 
 uint64_t rpi_st_read()
 {
-    uint64_t v = rpi_sys_timer->CHI;
-    return (v << 32 | rpi_sys_timer->CLO);
+    uint64_t v = rpi_sys_timer->CHI.reg;
+    return (v << 32 | rpi_sys_timer->CLO.reg);
 }
