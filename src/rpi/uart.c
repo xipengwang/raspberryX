2249c1cdb7e6043831713409a1831a06cfd7140c
Xipeng Wang <xipengw@umich.edu>
2017-04-06 16:54:19 -0400

    WIP

--- src/rpi/uart.c
+++ src/rpi/uart.c
@@ -2,7 +2,8 @@
 
 volatile rpi_uart_t *rpi_uart;
 
-int rpi_uart_init(volatile rpi_uart_t *rpi_uart){
+int rpi_uart_init(volatile rpi_uart_t *rpi_uart)
+{
 
     if(rpi_uart == MAP_FAILED){
 	return -1;
@@ -11,7 +12,7 @@ int rpi_uart_init(volatile rpi_uart_t *rpi_uart){
     rpi_uart->CR.reg =0;
     //wait for the end of tx/rx of current char
     //flush trans FIFO  UART_LCRH.FEN =0
-    rpi_uart->LCRH.bit.FEN=0; 
+    rpi_uart->LCRH.bit.FEN=0;
     //transmit enable
     rpi_uart->CR.bit.TXE = 1;
     //receive enable
@@ -20,10 +21,10 @@ int rpi_uart_init(volatile rpi_uart_t *rpi_uart){
     rpi_uart->CR.bit.RTS = 1;
     //data is requested when there is space in the receive FIFO
     rpi_uart->CR.bit.RTSEN = 1;
-    
+
     rpi_uart->CR.bit.CTSEN = 1;
 
-    //enable FIFO 
+    //enable FIFO
     rpi_uart->LCRH.bit.FEN =1;
     //enable parity
     rpi_uart->LCRH.bit.PEN =1;
@@ -32,7 +33,8 @@ int rpi_uart_init(volatile rpi_uart_t *rpi_uart){
     return 0;
 }
 
-void rpi_uart_set_clock(volatile rpi_uart_t* rpi_uart, uint16_t _divider){
+void rpi_uart_set_clock(volatile rpi_uart_t* rpi_uart, uint16_t _divider)
+{
     uint16_t divider = _divider;
     if(divider & (divider -1)) {
       //not a power of two
@@ -44,12 +46,12 @@ void rpi_uart_set_clock(volatile rpi_uart_t* rpi_uart, uint16_t _divider){
     rpi_uart->IBRD.bit.IBRD = divider;
 }
 
-
-void rpi_uart_transmit(volatile rpi_uart_t* rpi_uart, char* tbuf, uint32_t len){
+void rpi_uart_transmit(volatile rpi_uart_t* rpi_uart, char* tbuf, uint32_t len)
+{
     uint32_t TXCnt =0;
 
     while (TXCnt < len){
-      
+
       if(!rpi_uart->FR.bit.TXFF){
          rpi_uart->DR.bit.DATA = tbuf[TXCnt];
          TXCnt++;
@@ -69,9 +71,10 @@ void rpi_uart_transmit(volatile rpi_uart_t* rpi_uart, char* tbuf, uint32_t len){
     }
 }
 
-void rpi_uart_receive(volatile rpi_uart_t* rpi_uart, char* rbuf, uint32_t len){
+void rpi_uart_receive(volatile rpi_uart_t* rpi_uart, char* rbuf, uint32_t len)
+{
     uint32_t RXCnt = 0;
- 
+
     while(RXCnt < len){
 	if(!rpi_uart->FR.bit.RXFF){
             rbuf[RXCnt] = rpi_uart->DR.bit.DATA;
@@ -91,7 +94,3 @@ void rpi_uart_receive(volatile rpi_uart_t* rpi_uart, char* rbuf, uint32_t len){
         }
     }
 }
-
-
-
-  
