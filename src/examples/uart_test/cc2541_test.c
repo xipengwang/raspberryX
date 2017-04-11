f48b5bdfdb2b1397f1271ccfa5e00ab71fcfb94b
Xipeng Wang <xipengw@umich.edu>
2017-04-11 12:29:14 -0400

    add cc2541

---
+++ src/examples/uart_test/cc2541_test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "common/print_util.h"
+#include "common/time_util.h"
+#include "rpi/rpi_all.h"
+
+
+// This is example is tested when PIN_08 is connected with PIN_10;
+int main(int argc, char **args)
+{
+    if(rpi_init() ==-1){
+        printf("initialization failed");
+        return -1;
+    }
+
+    //configure GPIO pin
+    rpi_gpio_fsel(PIN_08, RPI_GPIO_FSEL_ALT0);
+    rpi_gpio_fsel(PIN_10, RPI_GPIO_FSEL_ALT0);
+
+    //Default baudrate is 9600 bits/s
+    rpi_uart_init(rpi_uart);
+
+    while(1){
+        //send string
+        char *buf = "AT+ADDR?";
+        rpi_uart_transmit(rpi_uart, buf, strlen(buf));
+        printf("Send string:  %s \n", buf);
+
+        //receive string character by character
+        printf("Received string:  ");
+        char c;
+        while ((c = rpi_uart_getc(rpi_uart))) {
+            printf("%c", c);
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
