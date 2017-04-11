3d42abd457d034c858c7c0bfc08aaeb75c8c9a7d
Xipeng Wang <xipengw@umich.edu>
2017-04-11 17:59:55 -0400

    Get bluetooth cc2451 working

--- src/examples/uart_test/cc2541_test.c
+++ src/examples/uart_test/cc2541_test.c
@@ -22,11 +22,46 @@ int main(int argc, char **args)
     //Default baudrate is 9600 bits/s
     rpi_uart_init(rpi_uart);
 
-    //send string
-    char *buf = "AT+NAME?";
-    rpi_uart_transmit(rpi_uart, buf, strlen(buf));
-    printf("Send string:  %s \n", buf);
-    sleep(3);
+    /*
+     * wake up
+     in sleep mode, you can send a long string (Length > 80 or more),
+     that string can made module wake up, and you will receive;
+     “OK+WAKE” string through UART. That string can’t include any AT commands; */
+    if(0) {
+        char *buf = "Please wake up! Please wake up! Please wake up! Please wake up! Please wake up! Please wake up!";
+        rpi_uart_transmit(rpi_uart, buf, strlen(buf));
+        printf("Send string:  %s \n", buf);
+        sleep(3);
+    }
+
+    {
+        //check name
+        char *buf = "AT+NAME?";
+        rpi_uart_transmit(rpi_uart, buf, strlen(buf));
+        sleep(1);
+        printf("Device name:  ");
+        char c;
+        while ((c = rpi_uart_getc(rpi_uart))) {
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+
+    {
+        char *buf = "AT+ADDR?";
+        rpi_uart_transmit(rpi_uart, buf, strlen(buf));
+        sleep(1);
+        printf("Device MAC:  ");
+        char c;
+        while ((c = rpi_uart_getc(rpi_uart))) {
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+    //check mac address
+
+
+
     while(1){
         //receive string character by character
         printf("Received string:  ");
